8417831051aa7291b267497386f64137063a0f4d
MethylSatan <methylsatan@gmail.com>
2023-12-16 01:46:06 +0900

    fix file path / fix PaifuURL format

--- Tampermonkey/GetPaifuURLs.js
+++ Tampermonkey/GetPaifuURLs.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GetPaifuURLs
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0
 // @description  try to take over the world!
 // @author       Buntaicho
 // @match        https://game.mahjongsoul.com/*
@@ -9,23 +9,23 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
     const MAX_URL_COUNT = 30; //牌譜URL取得件数、変更可
     const TYPE = 1; // type 1 友人 type 2 マッチング　type 4 大会戦 type 0 段位・友人・大会全て
     const KEY = 76; // デフォルトではLキー(=76)を設定済み、変更する場合は→で調べる https://keycode.info/
 
-    document.addEventListener("keydown", function(e) {
+    document.addEventListener("keydown", function (e) {
         e = e || window.event;
-        if (KEY == e.keyCode || KEY == e.key){
+        if (KEY == e.keyCode || KEY == e.key) {
             getRecords(TYPE);
         }
     });
 
-    function download(data, fileName) {
-        console.log("data: " + data);
+    function download(text, fileName) {
+        console.log("text: " + text);
         let a = document.createElement("a");
-        a.href = URL.createObjectURL(new Blob([data],{type: "text/plain"}));
+        a.href = URL.createObjectURL(new Blob([text], { type: "text/plain" }));
         a.download = fileName;
         a.style.display = "none";
         document.body.appendChild(a);
@@ -34,26 +34,29 @@
     }
 
     async function getRecords(type) {
-        let urls = "";
+        let output = {};
+        output.records = [{}];
         app.NetAgent.sendReq2Lobby(
             "Lobby",
             "fetchGameRecordList",
-            {start:0, count:MAX_URL_COUNT, type:type},
-            function(error, Records) {
+            { start: 0, count: MAX_URL_COUNT, type: type },
+            function (error, Records) {
                 if (Records.record_list.length == 0) {
                     return;
                 }
                 console.log("データ取得中 / Downloading");
-                for (const record of Records.record_list) {
-                    urls += "https://game.mahjongsoul.com/?paipu=" + record.uuid + "\n";
-                    urls += " " + new Date(record.start_time * 1000).toLocaleString();
-                    for (const account of record.accounts) {
-                        urls += " " + account.nickname;
+                console.log(Records.record_list.length)
+                for (let i = 0; i < Records.record_list.length; i++) {
+                    output.records[i] = {};
+                    let url = "https://game.mahjongsoul.com/?paipu=" + Records.record_list[i].uuid;
+                    let meta = " " + new Date(Records.record_list[i].start_time * 1000).toLocaleString();
+                    for (const account of Records.record_list[i].accounts) {
+                        meta += " " + account.nickname;
                     }
-                    urls += "\n"
-                    console.log(record);
+                    output.records[i].url = url;
+                    output.records[i].meta = meta;
                 }
-                download(urls, "PaifuURLs_" + new Date().toISOString().slice(0, 10).replace(/-/g, '') + ".txt");
+                download(JSON.stringify(output, null, "\t").replaceAll("\t", ""), "PaifuURLs_" + new Date().toISOString().slice(0, 10).replace(/-/g, "") + ".txt");
                 return;
             });
     }
